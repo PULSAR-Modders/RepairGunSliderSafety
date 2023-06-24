ef5ceff151024a4acaa12f782cbfe45f8a25ff55
18107 <18107@users.noreply.github.com>
2023-06-24 19:17:48 +0930

    Add files via upload

---
+++ Mod.cs
@@ -0,0 +1,22 @@
+﻿using PulsarModLoader;
+
+namespace Repair_gun_slider_safety
+{
+    public class Mod : PulsarMod
+    {
+        public override string Version => "0.0.1";
+
+        public override string Author => "18107";
+
+        public override string ShortDescription => "Prevents system power sliders from being moved if a Repair Gun, Repair Grenade Launcher, or FB MultiTool is equipped.";
+
+        public override string Name => "Repair gun slider safety";
+
+        public override string ModID => "repairgunslidersafety";
+
+        public override string HarmonyIdentifier()
+        {
+            return "id107.repairgunslidersafety";
+        }
+    }
+}
---
+++ ShipInfo.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+
+namespace Repair_gun_slider_safety
+{
+    [HarmonyPatch(typeof(PLShipInfo), "OnSysInstValueChanged")]
+    internal class ShipInfo
+    {
+        static bool Prefix()
+        {
+            string itemName = PLNetworkManager.Instance.LocalPlayer.MyInventory.ActiveItem.GetItemName(true);
+            if ((itemName.StartsWith("Repair Gun") || itemName.StartsWith("Repair Grenade Launcher") || itemName.StartsWith("FB MultiTool")) && !PLInput.Instance.GetButton(PLInputBase.EInputActionName.unlock_mouse))
+            {
+                return false; //skip method
+            }
+            return true; //allow method to run
+        }
+    }
+}
